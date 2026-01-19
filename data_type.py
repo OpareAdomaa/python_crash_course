2ea528978ebfa08cc26127e174254c739dca7111
OpareAdomaa <opareadomaa20@gmail.com>
2026-01-19 23:38:29 +0000

    Chapter two of crash course.

---
+++ data_type.py
@@ -0,0 +1,15 @@
+name = "Santa" # Variable to store name
+age = 20 # Variable to store age
+date_of_birth = 2025 - age # Calculating date of birth
+print(f"My name is {name.upper()} and I was Born in {date_of_birth} hence I'm {age} years old.") #Use of f-string and capitalization
+
+website = "www.google.com" # Variable to store website URL
+print(website.removeprefix("www.")) # Removing 'www.' prefix from the URL
+
+a = 3 # Variable assignment
+b = 5 # Variable assignment
+c = 7 # Variable assignment
+print("The sum of a, b and c is:", a + b + c) # Printing the sum of a, b and c
+print(a**5) # Printing a raised to the power of 5
+print(b**5) # Printing b raised to the power of 5
+print(c**5) # Printing c raised to the power of 5
